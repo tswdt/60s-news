37148e74605d87b6d4716f27f1923d83f27e8635
w3221540766 <53103680+tswdt@users.noreply.github.com>
2026-04-14 15:35:36 +0800

    Update background images

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,13 +14,13 @@ Page({
   audioContext: null,
 
   landscapeImages: [
-    'https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=800',
-    'https://images.unsplash.com/photo-1469474968028-56623f02e42e?w=800',
-    'https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?w=800',
-    'https://images.unsplash.com/photo-1433086966358-54859d0ed716?w=800',
-    'https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?w=800',
-    'https://images.unsplash.com/photo-1501854140801-50d01698950b?w=800',
-    'https://images.unsplash.com/photo-1426604966848-d7adac402bff?w=800'
+    'https://images.unsplash.com/photo-1507400492013-162706c8c05e?w=800',
+    'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?w=800',
+    'https://images.unsplash.com/photo-1501785888041-af3ef285b470?w=800',
+    'https://images.unsplash.com/photo-1472214103451-9374bd1c798e?w=800',
+    'https://images.unsplash.com/photo-1439066615861-d1af74d74000?w=800',
+    'https://images.unsplash.com/photo-1505765050516-f72dcac9c60e?w=800',
+    'https://images.unsplash.com/photo-1493246507139-91e8fad9978e?w=800'
   ],
 
   onLoad() {
